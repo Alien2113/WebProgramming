bc50739fde5e30bd6840b13a9ef981038416b6ce
bekowsky <bekowsky32@gmail.com>
2019-05-29 22:58:14 +0300

    controllers and little Views

---
+++ WebProgramming/FirstASP/FirstASP/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Web.Mvc;
+using FirstASP.Models;
+
+namespace FirstASP.Controllers
+{
+    public class UsersController : Controller
+    {
+        private ShopContext db = new ShopContext();
+
+      
+      
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,FIO,login,password,email,admin")] Users users)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (Users U in db.Userss)
+                {
+                    string text = U.login.Trim();
+                    if (text == users.login)
+                    {
+                        ModelState.AddModelError("Error", "Такой логин уже занят");
+                        return View(users);
+                    }
+                }
+
+                db.Userss.Add(users);
+                db.SaveChanges();
+                return RedirectToAction("Login","Home",new Users { admin = false, email = users.email, password = users.password, login = users.login, FIO = users.FIO });
+            }
+
+            return View(users);
+        }
+
+    
+    }
+
+}
